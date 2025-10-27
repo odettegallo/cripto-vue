5a53e32a36a888896ca9950d867c48889daec433
Odette Gallo <odette.gallo@hotmail.com>
2025-10-27 15:26:03 -0300

    estilos navbar

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,23 +1,23 @@
 <template>
   <div>
-    <v-app-bar class="app-bar-luxury" elevation="6"> 
+    <v-app-bar class="bg-black" elevation="2">
       <v-container>
         <div class="d-flex align-center w-100">
 
-          <v-app-bar-nav-icon class="d-lg-none text-secondary" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+          <v-app-bar-nav-icon class="d-lg-none text-white" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
 
-          <RouterLink class="navbar-brand fw-bold text-decoration-none d-flex align-center" to="/">
-            <v-icon class="me-2 text-secondary logo-icon">mdi-atom</v-icon>
-            <span class="text-gradient">Crypto Portal</span>
+          <RouterLink class="navbar-brand fw-bold text-white text-decoration-none d-flex align-center" to="/">
+            <v-icon class="me-2" color="white">mdi-currency-btc</v-icon>
+            Crypto Portal
           </RouterLink>
 
           <div class="d-none d-lg-flex align-center ms-6" v-if="isAuthenticated">
               <RouterLink class="nav-link text-white text-decoration-none" to="/home">
-                <v-icon class="me-1 text-primary">mdi-home-lightning-bolt-outline</v-icon>
+                <v-icon class="me-1" color="white">mdi-home</v-icon>
                 Inicio
               </RouterLink>
               <RouterLink v-if="isAdmin" class="nav-link text-white text-decoration-none ms-4" to="/admin">
-                <v-icon class="me-1 text-primary">mdi-shield-cog-outline</v-icon>
+                <v-icon class="me-1" color="white">mdi-cog</v-icon>
                 Administración
               </RouterLink>
           </div>
@@ -26,16 +26,16 @@
 
           <div class="d-flex align-center">
             <template v-if="isAuthenticated">
-              <span class="navbar-text me-3 d-none d-md-inline text-primary fw-bold text-truncate">
-                <v-icon color="secondary" size="18" class="me-1">mdi-account-star-outline</v-icon>
+              <span class="navbar-text me-3 d-none d-md-inline text-white fw-bold">
+                <v-icon color="white" size="18" class="me-1">mdi-account-circle</v-icon>
                 {{ userEmail }}
               </span>
-              <v-btn class="btn-neon-outline" variant="outlined" size="small" @click="handleLogout" prepend-icon="mdi-power-cycle">
+              <v-btn class="text-white" variant="outlined" size="small" @click="handleLogout" prepend-icon="mdi-logout">
                 Cerrar Sesión
               </v-btn>
             </template>
             <template v-else>
-               <v-btn class="btn-gold-flat" variant="flat" size="small" to="/login" prepend-icon="mdi-shield-key-outline">
+               <v-btn class="text-white" variant="outlined" size="small" to="/login" prepend-icon="mdi-login">
                 Ingresar
               </v-btn>
             </template>
@@ -44,23 +44,23 @@
       </v-container>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" temporary class="nav-drawer-dark">
+    <v-navigation-drawer v-model="drawer" temporary>
       <v-list dense nav>
-        <v-list-item class="bg-surface mb-3">
-          <v-list-item-title class="fw-bold">
-            <v-icon class="me-2 text-secondary">mdi-atom</v-icon> <span class="text-gradient">Crypto Portal</span>
+        <v-list-item class="bg-purple mb-3">
+          <v-list-item-title class="fw-bold text-white">
+            <v-icon class="me-2" color="white">mdi-currency-btc</v-icon> Crypto Portal
           </v-list-item-title>
         </v-list-item>
         
-        <v-list-item prepend-icon="mdi-home-lightning-bolt-outline" title="Inicio" to="/home" @click="drawer = false" color="primary" />
+        <v-list-item prepend-icon="mdi-home" title="Inicio" to="/home" @click="drawer = false" />
         
-        <v-list-item v-if="isAdmin" prepend-icon="mdi-shield-cog-outline" title="Administración" to="/admin" @click="drawer = false" color="primary" />
+        <v-list-item v-if="isAdmin" prepend-icon="mdi-cog" title="Administración" to="/admin" @click="drawer = false" />
         
-        <v-list-item v-if="isAuthenticated" prepend-icon="mdi-power-cycle" title="Cerrar Sesión" @click="handleLogout" color="secondary" />
-        <v-list-item v-else prepend-icon="mdi-shield-key-outline" title="Ingresar" to="/login" @click="drawer = false" color="primary" />
+        <v-list-item v-if="isAuthenticated" prepend-icon="mdi-logout" title="Cerrar Sesión" @click="handleLogout" />
+        <v-list-item v-else prepend-icon="mdi-login" title="Ingresar" to="/login" @click="drawer = false" />
         
         <v-list-item v-if="isAuthenticated">
-            <v-list-item-title class="text-medium-emphasis text-caption mt-2">Usuario: <span class="text-primary">{{ userEmail }}</span></v-list-item-title>
+            <v-list-item-title class="text-muted text-caption mt-2">Usuario: {{ userEmail }}</v-list-item-title>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
